85fdb8c43026c80c17e8082ad3a84bfe6f8b9ff0
DrTim105 <Salihutimothy52@gmail.com>
2021-12-29 23:54:18 +0100

    added app id and key

---
+++ app/src/main/java/com/salihutimothy/oxforddictionaryapp/MyDictionaryRequest.kt
@@ -0,0 +1,17 @@
+package com.salihutimothy.oxforddictionaryapp
+
+import android.os.AsyncTask
+
+class MyDictionaryRequest : AsyncTask<Void, Void, Void>() {
+
+    val app_id = "aaa664ff"
+    val app_key = "ded32974b64492af1217ff773d22bdd3"
+
+    override fun doInBackground(vararg params: Void?): Void {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPostExecute(result: Void?) {
+        super.onPostExecute(result)
+    }
+}
